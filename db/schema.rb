113a53b01e459f4cde6753afad05f27880368aae
Dana Jones <djones@larkfarm.com>
2017-05-27 20:44:27 -0500

    Upgrade Rails version

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20150727181858) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "projects", force: true do |t|
+  create_table "projects", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "begin_time"
