ea3b5a171d478b754eb234f9c1a7ab948e37b64b
Dana Jones <djones@larkfarm.com>
2017-05-27 20:45:09 -0500

    Basic specs for project index page

---
+++ spec/features/project_viewing_spec.rb
@@ -0,0 +1,35 @@
+require "spec_helper"
+
+feature "Viewing the list of existing projects" do
+  scenario "Some projects already exist" do
+    project = create(:project, name: "ABC Project")
+    project2 = create(:project, name: "XYZ Project")
+
+    visit projects_path
+
+    expect(page).to have_content("Projects")
+
+    within("table#projects") do
+      within("tr#project_#{ project.id}") do
+        expect(page).to have_content("ABC Project")
+        expect(page).to have_css("img[title='Edit ABC Project']")
+      end
+      within("#project_#{ project2.id }") do
+        expect(page).to have_content("XYZ Project")
+        expect(page).to have_css("img[title='Edit XYZ Project']")
+      end
+    end
+
+    expect(page).to have_link("New Project")
+  end
+
+  scenario "There are no existing projects" do
+    visit "/projects"
+
+    expect(page).to have_content("Projects")
+
+    expect(page).not_to have_content("table#projects")
+    expect(page).to have_content("New Project")
+    expect(page).to have_content("There are currently no active projects.")
+  end
+end
