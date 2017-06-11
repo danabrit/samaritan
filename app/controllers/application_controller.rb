7081216ba02747f5584f50a02fecfe800c5c7038
Dana Jones <djones@larkfarm.com>
2017-06-11 14:46:15 -0500

    Added the path & params needed so a new user could create an account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit(:username, :first_name, :last_name)
+      user_params.permit(:username, :first_name, :last_name, :email, :password, :password_confirmation)
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   resources :projects
 
   devise_scope :user do
-    get 'sign_in', to: 'devise/sessions#new'
-    get 'sign_up', to: 'devise/registrations#new'
+    get 'sign_in',      to: 'devise/sessions#new'
+    get 'sign_up',      to: 'devise/registrations#new'
+    delete 'sign_out',  to: 'devise/sessions#destroy'
   end
 
   root "projects#index"
