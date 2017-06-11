fe424813b030a7d5acf19aa4ac772b11636e8683
Dana Jones <djones@larkfarm.com>
2017-06-11 11:27:49 -0500

    Initial Bootstrap styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :projects
 
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+    get 'sign_up', to: 'devise/registrations#new'
+  end
+
   root "projects#index"
 end
