354a7d45287cb6ad84b00581b826a3ff35caa014
Dana Jones <djones@larkfarm.com>
2018-07-16 10:00:01 -0500

    More standardizing spacing and single-quotes

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -25,22 +25,22 @@
 
 FactoryBot.define do
   factory :project do
-    name              "Park Cleanup"
-    description       "The park needs some love. Let's clean it up!"
-    begin_time        "2014-03-20 17:40:08"
-    end_time          "2014-03-20 17:40:08"
-    address_1         "Pine Wood Park"
-    city              "Phoenix"
-    country           "US"
-    time_zone         "Central Time (US & Canada)"
-    supplies_required "Gloves and trash bags"
-    children_allowed  true
-    sponsoring_org    "The Clean Parks Committee"
-    max_signups       20
+    name 'Park Cleanup'
+    description "The park needs some love. Let's clean it up!"
+    begin_time '2014-03-20 17:40:08'
+    end_time '2014-03-20 17:40:08'
+    address_1 'Pine Wood Park'
+    city 'Phoenix'
+    country 'US'
+    time_zone 'Central Time (US & Canada)'
+    supplies_required 'Gloves and trash bags'
+    children_allowed true
+    sponsoring_org 'The Clean Parks Committee'
+    max_signups 20
     owner
   end
 
   factory :invalid_project, class: Project do
-    name              nil
+    name nil
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -28,12 +28,12 @@
 
 FactoryBot.define do
   factory :user, aliases: [:owner] do
-    first_name                    "Example"
-    sequence(:last_name)          { |n| "Person#{n}" }
-    sequence(:email)              { |n| "user#{n}@example.com" }
-    password                      "password"
-    sequence(:confirmation_token) { |n| "token#{n}" }
-    confirmed_at                  Time.zone.today
-    confirmation_sent_at          Time.zone.yesterday
+    first_name 'Example'
+    sequence(:last_name) { |n| "Person#{ n }" }
+    sequence(:email) { |n| "user#{ n }@example.com" }
+    password 'password'
+    sequence(:confirmation_token) { |n| "token#{ n }" }
+    confirmed_at Time.zone.today
+    confirmation_sent_at Time.zone.yesterday
   end
 end
